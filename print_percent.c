0417dd08dd3a83ad5ee25d7648f129698c05b606
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-16 04:55:06 +0000

    Editing percent

--- print_percent.c
+++ print_percent.c
@@ -3,11 +3,15 @@
 /**
  * print_percent - Print a percent sign.
  *
+ * @args: List of arguments.
+ *
  * Return: The number of characters printed.
  */
 
-int print_percent(void)
+int print_percent(va_list args)
 {
+	(void) args;
+
 	_putchar('%');
 
 	return (1);
