9886cab981cb426ffe6319a197d29439c4e472a7
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-17 00:00:46 +0000

    Error

--- more_functions.c
+++ more_functions.c
@@ -27,7 +27,7 @@ int print(const char *format, va_list args, type specifiers[])
 			if (format[i + 1] == ' ')
 				i++;
 
-			while (j < 10)
+			while (j < 14 && !format[i])
 			{
 				if (format[i + 1] == specifiers[j].op[0])
 				{
@@ -35,7 +35,7 @@ int print(const char *format, va_list args, type specifiers[])
 					i++;
 					break;
 				}
-				if (j == 9 && format[i + 1] != specifiers[j].op[1])
+				if (j == 13 && format[i + 1] != specifiers[j].op[1])
 				{
 					if (!format[i + 1])
 						return (-1);
